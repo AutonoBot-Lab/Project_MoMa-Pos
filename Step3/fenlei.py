a63a4c11e1fc83c72dde5697312d2e2adbb8cd5d
sbc <94527509+ssspeg@users.noreply.github.com>
2023-12-19 20:55:05 +0800

    Add files via upload

--- Step3/fenlei.py
+++ Step3/fenlei.py
@@ -24,42 +24,12 @@ def classify_urdf_joints(urdf_content):
     else:
         return "unknown"
 
-def count_urdf_joints(urdf_content):
-    # 解析XML内容
-    root = ET.fromstring(urdf_content)
-
-    # 初始化关节类型计数器
-    joint_types = {"fixed": 0, "revolute": 0, "prismatic": 0}
-
-    # 遍历所有关节并计数
-    for joint in root.findall('.//joint'):  # 使用 './/joint' 来确保可以找到所有级别的joint标签
-        joint_type = joint.get('type')  # 获取type属性
-        if joint_type in joint_types:
-            joint_types[joint_type] += 1
-
-    return joint_types
 # 使用函数
-
-def print_revolute_prismatic_joints(urdf_content):
-    # 解析XML内容
-    root = ET.fromstring(urdf_content)
-
-    # 遍历所有关节并打印revolute和prismatic关节的信息
-    for joint in root.findall('.//joint'):
-        joint_type = joint.get('type')
-        if joint_type in ["revolute", "prismatic"]:
-            child_link = joint.find('child').get('link')
-            parent_link = joint.find('parent').get('link')
-            print(f"Joint name: {joint.get('name')}, Type: {joint_type}, Child link: {child_link}, Parent link: {parent_link}")
-# /home/shaobeichen/robot/BestMan-step3/BestMan_Pybullet-master/Kitchen_models//models/Dishwasher/2085/mobility.urdf
-urdf_file_path = '/home/shaobeichen/robot/BestMan-step3/BestMan_Pybullet-master/Kitchen_models//models/Fridge/10144/mobility.urdf'
+urdf_file_path = '/home/sc-5/Desktop/shao/step3/BestMan_Pybullet-master/BestMan_Pybullet-master/URDF_models/URDF_models-main/furniture_table_rectangle_high/table.urdf'
 with open(urdf_file_path, 'r') as file:
     urdf_content = file.read()
-
+# Table: /home/sc-5/Desktop/shao/step3/BestMan_Pybullet-master/BestMan_Pybullet-master/URDF_models/URDF_models-main/furniture_table_rectangle_high/table.urdf
+# Fridge:
 # 使用函数
 classification = classify_urdf_joints(urdf_content)
-joint_counts = count_urdf_joints(urdf_content)
-sp_links=print_revolute_prismatic_joints(urdf_content)
-print(f"Joint counts in the URDF model: {joint_counts}")
 print(f"The URDF model is classified as: {classification}")
-
--- Step3/revolute.py
+++ Step3/revolute.py
@@ -25,34 +25,20 @@ elif index == 1:
     from utils.utils_Kitchen_v1 import Kitchen
 else:
     assert False, "index should be 0 or 1"
-pb_client = PbClient(enable_GUI=False)
+pb_client = PbClient(enable_GUI=True)
 pb_client.enable_vertical_view(1.0, [1.7, 5.68, 1.95], -86.4, -52.3)
 pb_visualizer = PbVisualizer(pb_client)
 # logID = pb_client.start_record("example_manipulation") # start recording
-init_pose = Pose([1, 0, 0], [0.0, 0.0, math.pi / 2]) 
+init_pose = Pose([1, 0, 0], [0.0, 0.0, math.pi / 2])
 demo = Bestman(init_pose, pb_client)  # load robot
 demo.get_joint_link_info("arm")  # get info about arm
 init_joint = [0, -1.57, 2.0, -1.57, -1.57, 0]
 demo.move_arm_to_joint_angles(init_joint)  # reset arm joint position
 all_box_range=[]
-revolute_box_after=[]
-revolute_box_before=[]
 # load kitchen
 kitchen = Kitchen(pb_client)
-num_joints = p.getNumJoints(kitchen.elementE_id)
-link_revolute_id=None
-for i in range(num_joints):
-    joint_info=p.getJointInfo(kitchen.elementE_id,i)
-    link_name=joint_info[12].decode('UTF-8')
-    if link_name == 'door_frame':
-        link_revolute_id = i
-        break 
-print("Link ID for door frame :",link_revolute_id)
-range_before= pb_visualizer.draw_aabb_range(kitchen.elementE_id, link_revolute_id)
-revolute_box_before.append(range_before)
 kitchen.open_it('elementE', 1)
-range_after= pb_visualizer.draw_aabb_range(kitchen.elementE_id, link_revolute_id)
-revolute_box_after.append(range_after)
+num_joints = p.getNumJoints(kitchen.elementE_id)
 # pb_visualizer.draw_aabb_link(kitchen.elementE_id, num_joints-1)
 for i in range(0,num_joints):
     pb_visualizer.draw_aabb_link(kitchen.elementE_id, i)
@@ -60,9 +46,6 @@ for i in range(0,num_joints):
     all_box_range.append(range_links)
     print(i,': num_joint range is:',range_links)
 print('All boxes range are:',all_box_range)
-# 创建一个映射关节名称到ID的字典
-print("revolute_box_before :",revolute_box_before)
-print("revolute_box_after :",revolute_box_after)
 pb_client.wait(100)
 pb_client.disconnect_pybullet()
 
